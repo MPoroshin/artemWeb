4959e164a0b0c32ab56b7810c79c2d15b8cda3cc
Huawei <poroshin.34z@gmail.com>
2023-11-21 19:22:54 +0300

    пкупкупкуп

--- BagReportSite/SendReport/admin.py
+++ BagReportSite/SendReport/admin.py
@@ -5,4 +5,4 @@ from .models import BagReport
 @admin.register(BagReport)
 class BagReportAdmin(admin.ModelAdmin):
     list_display = ("name_user", "date_added", 'active', 'id')
-    #абобefewfewf
+    #абобefewfewfауцауцауцацуацуа
