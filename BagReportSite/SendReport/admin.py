bfdc2e637a9ebe8ce42ebe4d7d8c56296f67f532
Y********* <y*********@gmail.com>
2023-11-21 20:29:15 +0300

    пкупкуп

--- BagReportSite/SendReport/admin.py
+++ BagReportSite/SendReport/admin.py
@@ -6,6 +6,6 @@ from .models import BagReport
 class BagReportAdmin(admin.ModelAdmin):
     list_display = ("name_user", "date_added", 'active', 'id')
     #абобefewfewfауцауцауцацуацуа
-    # коммент в ветке
-    #aboba1
+    # коммент в ветке232323213
+    #aboba12323
 
